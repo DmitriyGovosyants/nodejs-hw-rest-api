bd3f148b98c60eb05b1758839caaa8fc88b997a1
DmitriyGovosyants <Demiangvs@gmail.com>
2022-10-16 10:36:37 +0300

    refact: add check token exist to auth

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,8 +7,8 @@ const { SECRET_KEY } = process.env
 const authenticate = async (req, res, next) => {
   try {
     const { authorization = "" } = req.headers;
-    const [bearer = "", token = ""] = authorization.split(" ");
-    if (bearer !== "Bearer") {
+    const [bearer = "", token] = authorization.split(" ");
+    if (!token || bearer !== "Bearer") {
       throw RequestError(401, "Not authorized")
     }
 
